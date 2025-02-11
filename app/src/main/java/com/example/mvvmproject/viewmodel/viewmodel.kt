16172747f7d94c4492ec01b5bc79281588a61f40
karthik <your_email@example.com>
2025-02-11 17:28:38 +0530

    i just removed the space

--- app/src/main/java/com/example/mvvmproject/viewmodel/viewmodel.kt
+++ app/src/main/java/com/example/mvvmproject/viewmodel/viewmodel.kt
@@ -7,7 +7,6 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 
 import com.example.mvvmproject.model.Item
-
 class ItemViewModel : ViewModel() {
     var items = mutableStateListOf<Item>()
         private set
